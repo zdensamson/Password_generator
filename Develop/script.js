460dafb41e0ca729a30ccf89aef081c3978f10d4
Deku <zachdsamson@gmail.com>
2021-07-03 11:49:05 -0700

    ask user what character types they want

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,8 @@
 // Assignment code here
 function generatePassword() {
-  var passLen = lengthCheck();
+  // stores the desired length of password
+  passLen = lengthCheck();
+  charList = charType();
   
 };
 
@@ -30,6 +32,22 @@ function lengthCheck() {
 // lengthCheck calls this function to prompt user input for password length
 function lengthPrompt() {
   return window.prompt("Please select the length of your password: 8-128 characters accepted.");
+}
+
+// ask yes/no for each possible character type 
+function charType() {
+  lowerCaseCheck = window.confirm("Would you like LOWER CASE letters in your password?");
+  upperCaseCheck = window.confirm("Would you like UPPER CASE letters in your password?");
+  numberCheck = window.confirm("Would you like NUMBERS in your password?");
+  specialCheck = window.confirm("Would you like SPECIAL CHARACTERS in your password? (i.e. !, $, etc.)")
+  // store the 4 seperate answers in an array 
+  charList = [lowerCaseCheck, upperCaseCheck, numberCheck, specialCheck];
+  // if user says "NO" to all char types -- re-run the function 
+  if (!lowerCaseCheck && !upperCaseCheck && !numberCheck && !specialCheck) {
+    window.alert("Please choose at least ONE character type for your password.")
+    charType();
+  }
+  return charList;
 }
 
 // Get references to the #generate element
