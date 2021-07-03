7d488d2a096329fc8cd0399820bfb2d02adf56f2
Deku <zachdsamson@gmail.com>
2021-07-02 17:13:31 -0700

    added function to ensure length of password is between 8 & 128

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,20 @@
 // Assignment code here
+function generatePassword() {
+  passLength = lengthCheck();
 
+};
+
+// check for length of password
+function lengthCheck() {
+  var passLength = window.prompt("Please select the length of your password: 8-128 characters accepted.");
+  passLength = parseInt(passLength);
+  
+  if (passLength < 8 || passLength > 128) {
+    window.alert("Please choose a number GREATER than 8 or LESS than 128.");
+    lengthCheck();
+  }
+  return passLength;
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +25,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
